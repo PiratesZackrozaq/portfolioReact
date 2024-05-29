2fe011091b48b1f89bf0998a9b87996d260e14c2
Muhammad Zaki Rozaqta <zackrozaq36@gmail.com>
2024-05-28 23:55:44 -0800

    Add files via upload

--- components/NavMobile.tsx
+++ components/NavMobile.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 interface Props {
   showNav: boolean;
-  closeNav: () => void
+  closeNav: () => void;
 }
 
 const NavMobile = ({ closeNav, showNav }: Props) => {
@@ -12,8 +12,9 @@ const NavMobile = ({ closeNav, showNav }: Props) => {
 
   return (
     <div>
-      <div className={'fixed top-0 ${navOpenStyle} transform transition-all duration-500 z-[10000] left-0 ring-0 bottom-0 bg-black opacity-70 w-[100vw] h-[100vh]'}>
-        <ul className={'text-white ${navOpenStyle} fixed flex items-center flex-col justify-center h-[100%] transform transition-all duration-300 delay-300 w-[60%] bg-red-600 space-y-14 z-[10006]'}>
+      <div className={'fixed top-0 ${ navOpenStyle } transform transition-all duration-500 z-[10000] left-0 ring-0 bottom-0 bg-black opacity-70 w-[100vw] h-[100vh]'}>
+      </div>
+        <ul className={'text-white ${ navOpenStyle } fixed flex items-center flex-col justify-center h-[100%] transform transition-all duration-300 delay-300 w-[60%] bg-red-600 space-y-14 z-[10006]'}>
           <li><a className="nav__link text-[25px] sm:text-[30px]" href="#">Home</a></li>
           <li><a className="nav__link text-[25px] sm:text-[30px]" href="#">About</a></li>
           <li><a className="nav__link text-[25px] sm:text-[30px]" href="#">Services</a></li>
@@ -21,7 +22,6 @@ const NavMobile = ({ closeNav, showNav }: Props) => {
           <li><a className="nav__link text-[25px] sm:text-[30px]" href="#">Contact</a></li>
           <XMarkIcon onClick={closeNav} className="absolute top-[-1.4rem] right-[1.4rem] w-[2.2rem] h-[2.2rem] text-white" />
         </ul>
-      </div>
     </div>
   );
 };
