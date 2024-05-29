8c0d0c30139102bbd7e4c7f4064deb943ee84a22
PiratesZackrozaq <zackrozaq36@gmail.com>
2024-05-28 19:26:43 -0800

    Deploy

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Nav from "@/components/Nav";
+import NavMobile from "@/components/NavMobile";
+import React, { useState } from "react";
+
+const Homepage = () => {
+    const [showNav, setShowNav] = useState(false);
+    const showNavHandler = () => setShowNav(true);
+    const closeNavHandler = () => setShowNav(false);
+
+    return (
+        <div className="overflow-hidden">
+            <NavMobile showNav={showNav} closeNav={closeNavHandler} />
+            <Nav openNav={showNavHandler} />
+        </div>
+    );
+};
+
+export default Homepage;
