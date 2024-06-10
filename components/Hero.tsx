64a8474a0421406f4875dc26ab47b7d1298a03bb
Muhammad Zaki Rozaqta <zackrozaq36@gmail.com>
2024-06-10 04:21:33 -0800

    Add files via upload

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TypeWriterEffect from './Helper/TypeWriterEffect';
+import Image from 'next/image';
 
 const Hero = () => {
     return (
@@ -14,6 +15,20 @@ const Hero = () => {
                             </h1>
                             <TypeWriterEffect />
                         </div>
+                        <p className='mt-[1.2rem] text-[15px] md:text-[17px] text-[#c4cfde]'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta qui repellat rem corporis mollitia corrupti sequi commodi omnis, aliquid, placeat, alias totam error id consequatur. Facere fugiat nulla veritatis veniam.
+                        </p>
+                        <div className='mt-[2rem] flex items-center space-x-6'>
+                            <button className='relative flex h-[50px] w-40 items-center justify-center font-semibold overflow-hidden bg-red-500 text-white shadow-2xl transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-blue-600 before:duration-500 before:ease-out hover:before:h-56 hover:before:w-56'>
+                                <span className='relative z-10'>My LinkedIn</span>
+                            </button>
+                            <button className='before:ease relative h-12 w-40 overflow-hidden bg-orange-600 text-white font-semibold shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-green-700 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180'>
+                                <span className='relative z-10'>Download CV</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div className='hidden md:block'>
+                        <Image src="/images/pirate.png" alt="pirate" width={600} height={600} className='object-contain' />
                     </div>
                 </div>
             </div>
---
+++ components/Services.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className='pt-[5rem] pb-[3rem] bg-[#0b0c13]'>
+        <div className='text-center'>
+            <p className='heading__mini'>My Services</p>
+            <h1 className='heading__primary'>My Best <span className='text-yellow-300'>Services</span></h1>
+        </div>
+    </div>
+  )
+}
+
+export default Services
