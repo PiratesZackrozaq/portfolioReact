2e08815e03972b561b2b24f74bcdf5028e7e5b41
Muhammad Zaki Rozaqta <zackrozaq36@gmail.com>
2024-06-13 04:21:58 -0800

    Add files via upload

--- components/Helper/ReviewSlider.tsx
+++ components/Helper/ReviewSlider.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import ClientReviewCard from './ClientReviewCard';
+import $ from 'jquery';
+
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1300 },
+        items: 3,
+        slidesToSlide: 1
+    },
+    tablet: {
+        breakpoint: { max: 1300, min: 764 },
+        items: 2,
+        slidesToSlide: 1
+    },
+    mobile: {
+        breakpoint: { max: 764, min: 0 },
+        items: 1,
+        slidesToSlide: 1
+    }
+};
+
+
 
 const ReviewSlider = () => {
-  return (
-    <div>ReviewSlider</div>
-  )
-}
+
+    return (
+        <Carousel
+            responsive={responsive}
+            additionalTransfrom={0} // means to render carousel on server-side.
+            arrows={true}
+            infinite={true}
+            autoPlay={true}
+            autoPlaySpeed={5000}
+            centerMode={false}
+            infinite={true}
+            itemClass="item"
+        >
+            <ClientReviewCard />
+        </Carousel>
+    )
+};
+
 
 export default ReviewSlider
