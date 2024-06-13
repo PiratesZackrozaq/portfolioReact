a3b176b8dd7226c67f353944b7e0361e0357bb28
Muhammad Zaki Rozaqta <zackrozaq36@gmail.com>
2024-06-13 05:54:35 -0800

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,118 +1,115 @@
-import {
-    Box,
-    FooterContainer,
-    Row,
-    Column,
-    FooterLink,
-    Heading,
-} from "./FooterStyles";
- 
+import React from "react";
+
 const Footer = () => {
     return (
-        <Box>
-            <h1
-                style={{
-                    color: "green",
-                    textAlign: "center",
-                    marginTop: "10px",
-                }}
-            >
-                "Start where you are. Use what you have. Do what you can." - Arthur Ashe
-            </h1>
-            <FooterContainer>
-                <Row>
-                    <Column>
-                        <Heading>About Us</Heading>
-                        <FooterLink href="#">
-                            Aim
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Vision
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Testimonials
-                        </FooterLink>
-                    </Column>
-                    <Column>
-                        <Heading>Services</Heading>
-                        <FooterLink href="#">
-                            Writing
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Internships
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Coding
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Teaching
-                        </FooterLink>
-                    </Column>
-                    <Column>
-                        <Heading>Contact Us</Heading>
-                        <FooterLink href="#">
-                            Uttar Pradesh
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Ahemdabad
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Indore
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Mumbai
-                        </FooterLink>
-                    </Column>
-                    <Column>
-                        <Heading>Social Media</Heading>
-                        <FooterLink href="#">
-                            <i className="fab fa-facebook-f">
-                                <span
-                                    style={{
-                                        marginLeft: "10px",
-                                    }}
-                                >
-                                    Facebook
-                                </span>
-                            </i>
-                        </FooterLink>
-                        <FooterLink href="#">
-                            <i className="fab fa-instagram">
-                                <span
-                                    style={{
-                                        marginLeft: "10px",
-                                    }}
-                                >
-                                    Instagram
-                                </span>
-                            </i>
-                        </FooterLink>
-                        <FooterLink href="#">
-                            <i className="fab fa-twitter">
-                                <span
-                                    style={{
-                                        marginLeft: "10px",
-                                    }}
-                                >
-                                    Twitter
-                                </span>
-                            </i>
-                        </FooterLink>
-                        <FooterLink href="#">
-                            <i className="fab fa-youtube">
-                                <span
-                                    style={{
-                                        marginLeft: "10px",
-                                    }}
-                                >
-                                    Youtube
-                                </span>
-                            </i>
-                        </FooterLink>
-                    </Column>
-                </Row>
-            </FooterContainer>
-        </Box>
+        <div className="Box">
+            <div className="text-center pb-[3rem]">
+                <p className='heading__mini'>
+                    "Start where you are. Use what you have. Do what you can." - Arthur Ashe
+                </p>
+            </div>
+            <div className="FooterContainer">
+                <div className="Row">
+                    <div className="Column">
+                        <div className="Heading">About Us</div>
+                        <div className="FooterLink">
+                            <a href="#">Aim</a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">Vision</a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">Testimonials</a>
+                        </div>
+                    </div>
+                    <div className="Column">
+                        <div className="Heading">Services</div>
+                        <div className="FooterLink">
+                            <a href="#">Writing</a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">Internships</a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">Coding</a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">Teaching</a>
+                        </div>
+                    </div>
+                    <div className="Column">
+                        <div className="Heading">Contact Us</div>
+                        <div className="FooterLink">
+                            <a href="#">Jack Sparrow</a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">Blackbeard</a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">Whitebeard</a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">Trafagar Law</a>
+                        </div>
+                    </div>
+                    <div className="Column">
+                        <div className="Heading">Social Media</div>
+                        <div className="FooterLink">
+                            <a href="#">
+                                <i className="fab fa-facebook-f">
+                                    <span
+                                        style={{
+                                            marginLeft: "10px",
+                                        }}
+                                    >
+                                        Facebook
+                                    </span>
+                                </i>
+                            </a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">
+                                <i className="fab fa-instagram">
+                                    <span
+                                        style={{
+                                            marginLeft: "10px",
+                                        }}
+                                    >
+                                        Instagram
+                                    </span>
+                                </i>
+                            </a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">
+                                <i className="fab fa-twitter">
+                                    <span
+                                        style={{
+                                            marginLeft: "10px",
+                                        }}
+                                    >
+                                        Twitter
+                                    </span>
+                                </i>
+                            </a>
+                        </div>
+                        <div className="FooterLink">
+                            <a href="#">
+                                <i className="fab fa-youtube">
+                                    <span
+                                        style={{
+                                            marginLeft: "10px",
+                                        }}
+                                    >
+                                        Youtube
+                                    </span>
+                                </i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
